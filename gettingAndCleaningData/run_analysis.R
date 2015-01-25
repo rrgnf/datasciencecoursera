1cb2658896079adbf4487f8f56cb6ad64b0d436a
Ricardo Nunes <rrgnf@yahoo.com.br>
2015-01-25 19:49:51 -0300

    file ok.

--- gettingAndCleaningData/run_analysis.R
+++ gettingAndCleaningData/run_analysis.R
@@ -1,51 +1,92 @@
 #run_analysis.R
 
+#load libraries
+library(tidyr)
+library(dplyr)
+
+#loading features
+features <- read.table(file = './UCI HAR Dataset/features.txt',header = F,)
+l1 <- features[1,]
+l1$V2 <- factor("empty")
+features <- rbind(l1,features)
+rm(l1)
+#head(features)
+names(features) <- c("idf","Description")
+features <- mutate(features,d=paste(Description,idf))
+
+#replace these character "( ) , - " by "_" 
+
+features2 <- as.array(features$d)
+features2 <- str_replace_all(features2,"[(), -]","_")
+features2 <- str_replace_all(features2,"_+","_")
+
+#loAding activities
+activities <- read.table(file = './UCI HAR Dataset/activity_labels.txt',header = F,)
+names(activities) <- c("id","Activities")
+activities <- select(activities,Activities)
+
+#function to 'say' 'activity name' by 'activity number'
+nomeAtividade <- function(idAtividade){
+  as.character(activities[idAtividade,1]);
+}
+
 #reading test data from csv files
 s_test <- read.csv(".\\UCI HAR Dataset\\test\\subject_test.txt")
+x_test <- read.csv(".\\UCI HAR Dataset\\test\\X_test.txt")
 y_test <- read.csv(".\\UCI HAR Dataset\\test\\Y_test.txt")
+
+#reading train data from csv files
+s_train <- read.csv(".\\UCI HAR Dataset\\train\\subject_train.txt")
+x_train <- read.csv(".\\UCI HAR Dataset\\train\\X_train.txt")
+y_train <- read.csv(".\\UCI HAR Dataset\\train\\Y_train.txt")
 
 #setting column names
 names(s_test) <- "subject"
+names(x_test)  <- "data_set"
 names(y_test)  <- "y"
 
-#merging columns on test data
-subject_test   <- cbind(  s_test , y_test  )
-
-#removing old var
-rm(s_test)
-rm(y_test)
-
-#doing the same for train data
-
-s_train  <- read.csv(".\\UCI HAR Dataset\\train\\subject_train.txt")
-y_train  <- read.csv(".\\UCI HAR Dataset\\train\\Y_train.txt")
-
 names(s_train) <- "subject"
+names(x_train)  <- "data_set"
 names(y_train)  <- "y"
 
-subject_train   <- cbind(  s_train ,  y_train  )
+#merging columns on test data
+subject_test   <- cbind(  s_test , y_test, x_test  )
+
+subject_train   <- cbind(  s_train , y_train, x_train  )
+
+rm(s_test)
+rm(y_test)
+rm(x_test)
 
 rm(s_train)
 rm(y_train)
+rm(x_train)
 
-#merging test and train data
-dados <- rbind(subject_test,subject_train)
+subject <- rbind(subject_test,subject_train)
+rm(subject_test,subject_train)
+rm(features)
 
-rm(subject_test)
-rm(subject_train)
+x_tbl <- tbl_df(subject)
+x_tbl <- separate(x_tbl,data_set, features2, sep = "\\s+")
+x_tbl <- select (x_tbl,-empty_1)
+x_tbl <- mutate(x_tbl, Activitie = nomeAtividade(x_tbl$y) )
+x_tbl2 <- select(x_tbl,contains("mean",ignore.case = T))
+x_tbl2 <- mutate_each(x_tbl2,funs(as.character))
+x_tbl2 <- mutate_each(x_tbl2,funs(as.numeric))
+x_tbl3 <- select(x_tbl,contains("std",ignore.case = T))
+x_tbl3 <- mutate_each(x_tbl3,funs(as.character))
+x_tbl3 <- mutate_each(x_tbl3,funs(as.numeric))
 
-#load required libraries
-library(dplyr)
-library(tidyr)
+x_tbl4 <- select(x_tbl,subject)
+x_tbl5 <- select(x_tbl,Activitie)
+x_tbl  <- cbind(x_tbl4,x_tbl5,x_tbl2,x_tbl3)
 
-#creating tbl_df
-dados_tbl <- tbl_df(dados) 
+rm(x_tbl5,x_tbl4,x_tbl3,x_tbl2)
 
-rm(dados)
+x_tbl <- group_by(x_tbl,Activitie,subject)
 
-#calc mean and sd
-dados_resp <- summarise_each(dados_tbl,funs(mean,sd))
+result <- summarise_each(x_tbl,funs(mean,sd),
+                  (tBodyAcc_mean_X_1):(fBodyBodyGyroJerkMag_std_543))
 
 #writing csv result data
-write.csv(dados_resp, "dados_resp.csv",row.names=F)
-
+write.csv(result, "result.csv",row.names=F)
