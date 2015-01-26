5ce3fcd455f51694db4f17c1d44e1b36dfc91711
Ricardo Nunes <rrgnf@yahoo.com.br>
2015-01-25 21:28:10 -0300

    adding *.md and update coments on run_analysis.R

--- gettingAndCleaningData/run_analysis.R
+++ gettingAndCleaningData/run_analysis.R
@@ -4,6 +4,7 @@
 library(tidyr)
 library(dplyr)
 
+#Step 1 (readme)
 #loading features
 features <- read.table(file = './UCI HAR Dataset/features.txt',header = F,)
 l1 <- features[1,]
@@ -20,16 +21,19 @@ features2 <- as.array(features$d)
 features2 <- str_replace_all(features2,"[(), -]","_")
 features2 <- str_replace_all(features2,"_+","_")
 
+#Step 2 (readme)
 #loAding activities
 activities <- read.table(file = './UCI HAR Dataset/activity_labels.txt',header = F,)
 names(activities) <- c("id","Activities")
 activities <- select(activities,Activities)
 
+#Step 3 (readme)
 #function to 'say' 'activity name' by 'activity number'
 nomeAtividade <- function(idAtividade){
   as.character(activities[idAtividade,1]);
 }
 
+#Step 4 (readme)
 #reading test data from csv files
 s_test <- read.csv(".\\UCI HAR Dataset\\test\\subject_test.txt")
 x_test <- read.csv(".\\UCI HAR Dataset\\test\\X_test.txt")
@@ -62,31 +66,48 @@ rm(s_train)
 rm(y_train)
 rm(x_train)
 
+
 subject <- rbind(subject_test,subject_train)
 rm(subject_test,subject_train)
 rm(features)
 
+#Step 5 (readme)
 x_tbl <- tbl_df(subject)
+
+#Step 6 (readme)
 x_tbl <- separate(x_tbl,data_set, features2, sep = "\\s+")
 x_tbl <- select (x_tbl,-empty_1)
+
+#Step 7 (readme)
 x_tbl <- mutate(x_tbl, Activitie = nomeAtividade(x_tbl$y) )
+
+#Step 8 (readme)
 x_tbl2 <- select(x_tbl,contains("mean",ignore.case = T))
+#Step 9 (readme)
 x_tbl2 <- mutate_each(x_tbl2,funs(as.character))
 x_tbl2 <- mutate_each(x_tbl2,funs(as.numeric))
+
+#Step 8 (readme)
 x_tbl3 <- select(x_tbl,contains("std",ignore.case = T))
+#Step 9 (readme)
 x_tbl3 <- mutate_each(x_tbl3,funs(as.character))
 x_tbl3 <- mutate_each(x_tbl3,funs(as.numeric))
 
 x_tbl4 <- select(x_tbl,subject)
 x_tbl5 <- select(x_tbl,Activitie)
+
+#Step 10 (readme)
 x_tbl  <- cbind(x_tbl4,x_tbl5,x_tbl2,x_tbl3)
 
 rm(x_tbl5,x_tbl4,x_tbl3,x_tbl2)
 
+#Step 11 (readme)
 x_tbl <- group_by(x_tbl,Activitie,subject)
 
+#Step 12 (readme)
 result <- summarise_each(x_tbl,funs(mean,sd),
                   (tBodyAcc_mean_X_1):(fBodyBodyGyroJerkMag_std_543))
 
+#Step 13 (readme)
 #writing csv result data
 write.csv(result, "result.csv",row.names=F)
